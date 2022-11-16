6e50177e97bcf1e31a3c92b33b2694bf4a381b2d
wojtek024 <61975407+wojtek024@users.noreply.github.com>
2022-11-16 18:28:22 +0100

    Usuwanie pracowników

--- Employee.py
+++ Employee.py
@@ -3,13 +3,15 @@ import pandas as pd
 
 class Employee:
     list_of_employees = []
-    Min_salary = 3000
+    MIN_SALARY = 3000
 
     def __init__(self, first_name, last_name, salary):
         self.first_name = first_name
         self.last_name = last_name
         self.salary = int(salary)
-        Employee.list_of_employees.append({'first_name': first_name, 'last_name': last_name, 'salary': salary})
+        self.id = self.get_max_id() + 1
+        Employee.list_of_employees.append(
+            {'id': self.id, 'first_name': first_name, 'last_name': last_name, 'salary': salary})
 
     # dodawanie pracowników poprzez zaimportowanie pliku csv
 
@@ -18,6 +20,12 @@ class Employee:
         data = pd.read_csv(filename, *args, **kwargs)
         for index, row in data.iterrows():
             cls(row['first_name'], row['last_name'], row['salary'])
+
+    def get_max_id(self):
+        if len(Employee.list_of_employees) == 0:
+            return 0
+        else:
+            return pd.DataFrame(Employee.list_of_employees)["id"].max()
 
     def give_raise(self, amount=0):
         self.salary = + amount
@@ -28,7 +36,7 @@ class Employee:
 
     @salary.setter
     def salary(self, new_salary):
-        if new_salary < Employee.Min_salary:
+        if new_salary < Employee.MIN_SALARY:
             raise ValueError("Invalid salary")
         else:
             self._salary = new_salary
--- Program.py
+++ Program.py
@@ -1,45 +1,91 @@
 import pandas as pd
 import getpass as gp
+import time
+import tabulate
 from Employee import Employee
 from Manager import Manager
 
 
 def export_employees():
     df = pd.DataFrame(Employee.list_of_employees)
-    df.to_csv('employees.csv', header=True)
+    df.to_csv('employees.csv', header=True, index=False)
 
 
 class Program:
     is_active = None
 
     def main_menu(self):
-        print('1. Panel zarządzania pracownikami\n2. Wyloguj\n')
+        print('\n1. Panel zarządzania pracownikami\n2. Wyloguj\n')
         opcja_1 = input('Wybierz opcję: ')
         if opcja_1 == '1':
             print('\n' * 20)
             print('Wybierz opcję:\na) Wyświetl informacje o pracownikach\nb) Dodaj pracownika\n'
-                  'c) Usuń pracownika\nd) Ustaw wysokość bonusu\ne) Przyznaj podwyżkę jednemu z pracowników')
+                  'c) Edytuj pracownika\nd) Edytuj pracownika\ne) Inne')
             opcja_2 = input()
             if opcja_2 == 'a' or opcja_2 == 'a)':
                 print('\n' * 20)
-                print('pracownicy')
-                print(Employee.list_of_employees)
+                self.display_employees()
             elif opcja_2 == 'b' or opcja_2 == 'b)':
                 print('\n' * 20)
-                print('dodaj pracownika')
+                print('Dodaj pracownika')
                 first_name = input("Give first name: ")
                 last_name = input("Give last name: ")
                 salary = input("Give salary: ")
                 Employee(first_name, last_name, salary)
+                export_employees()
             elif opcja_2 == 'c' or opcja_2 == 'c)':
                 print('\n' * 20)
-                print('usuń pracownika')
+                self.display_employees()
+                employee_id = int(input("Wybierz id pracownika:"))
+                if employee_id == '':
+                    raise ValueError("Invalid employee id")
+                else:
+                    df_employees = pd.read_csv('employees.csv', header=0, index_col=0)
+                    # employee_data = df_employees.loc[employee_id].tolist() -- jest coś źle
+                    print('\n1. Zmień wysokość wynagrodzenia\n2. Zmień dane osobowe\n3. Przyznaj podwyżkę\n'
+                          '4. Usuń pracownika z bazy danych')
+                    opcja_3 = int(input("Wybierz opcję: "))
+
+                    if opcja_3 == 1:
+                        print('1')
+                    elif opcja_3 == 2:
+                        print('2')
+                    elif opcja_3 == 3:
+                        print('3')
+                    elif opcja_3 == 4:
+                        Employee.list_of_employees = [i for i in Employee.list_of_employees
+                                                      if not (i['id'] == employee_id)]
+                        export_employees()
+                    else:
+                        print('\nPodano złą cyfrę!')
+                    # employee = Employee(employee_data)
+
             elif opcja_2 == 'd' or opcja_2 == 'd)':
                 print('\n' * 20)
-                print('bonus')
+                self.display_employees()
+                employee_id = input("Wybierz id pracownika:")
+                if employee_id == '':
+                    raise ValueError("Invalid employee id")
+                else:
+                    df_employees = pd.read_csv('employees.csv', header=0, index_col=0)
+                    employee_data = df_employees.iloc[employee_id].tolist()
+                    print('\n1. Zmień wysokość wynagrodzenia\n2. Zmień dane osobowe\n3. Przyznaj podwyżkę\n')
+                    opcja_3 = input("Wybierz opcję: ")
+
+                    if opcja_3 == 1:
+                        print('1')
+                    elif opcja_3 == 2:
+                        print('2')
+                    elif opcja_3 == 3:
+                        print('3')
+                    else:
+                        print('\nPodano złą cyfrę!')
+                    # employee = Employee(employee_data)
             elif opcja_2 == 'e' or opcja_2 == 'e)':
                 print('\n' * 20)
-                print('podwyżka')
+                # print("Wybierz
+                # give_raise(input("Podaj kwotę podwyżki: "))
+                print('Podaj podwyżka')
             else:
                 print("Podano złą literę!")
         elif opcja_1 == '2':
@@ -55,6 +101,7 @@ class Program:
         if pwd == 'Wojtek':
             Program.is_active = True
             print('Zalogowano!')
+            time.sleep(1.5)
             print('\n' * 20)
             while Program.is_active:
                 self.main_menu()
@@ -62,7 +109,14 @@ class Program:
             print('You entered wrong password')
             return
 
+    def display_employees(self):
+        print('Dostępni pracownicy:')
+        df_employees = pd.read_csv('employees.csv', header=0, index_col=0)
+        print(df_employees.to_markdown(tablefmt='grid'))
 
+    def get_employees_df(self):
+        df_employees = pd.read_csv('employees.csv', header=0, index_col=0)
+        return df_employees
 
 if __name__ == '__main__':
     # Employee('Wojciech', 'Sajecki', 10000)
